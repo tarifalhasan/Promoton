514f02409cba27ef973be3091bbf42c523531695
tarifalhasan <tarialhasan999@gmail.com>
2023-04-26 07:56:26 +0600

    html5 qr code add

--- pages/scan.jsx
+++ pages/scan.jsx
@@ -1,29 +1,46 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Html5Qrcode } from 'html5-qrcode';
 import Layout from '@/components/Layout';
-import React, { useState } from 'react';
 
-import { QrReader } from 'react-qr-reader';
+export default function QrCodeScanner() {
+  const [data, setData] = useState(null);
+  const [isPaid, seIsPaid] = useState(false);
+  const [isNotPaid, seIsNotPaid] = useState(false);
+  const qrcodeRef = useRef(null);
 
-const Scanner = () => {
-  const [data, setData] = useState('No result please scan');
+  useEffect(() => {
+    const html5Qrcode = new Html5Qrcode('qrcode-reader');
+    html5Qrcode.start(
+      { facingMode: 'environment' },
+      { fps: 10, qrbox: 250 },
+      // qrCodeMessage => {
+      //   console.log(`QR code detected: ${qrCodeMessage}`);
+      //   setData(qrCodeMessage);
+
+      //   if (
+      //     qrCodeMessage ==
+      //     '✅ Pago! Valor Pago: R$ 90,00 Data do Pagamento: 10/10/23 Horário do Pagamento: 13:12:06'
+      //   ) {
+      //     seIsPaid(true);
+      //   } else if (qrCodeMessage == '❌ Não Pago! Valor em Aberto: R$ 90,00') {
+      //     seIsNotPaid(true);
+      //   }
+      // },
+      errorMessage => {
+        console.log(`Error: ${errorMessage}`);
+      }
+    );
+  }, []);
 
   return (
     <Layout>
-      <QrReader
-        constraints={(facingMode = 'user')}
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
-      <p className=" text-lg text-center font-normal">{data}</p>
+      <div className="relative overflow-hidden">
+        <div
+          id="qrcode-reader"
+          className="mx-auto bg-no-repeat "
+          ref={qrcodeRef}
+        ></div>
+      </div>
     </Layout>
   );
-};
-
-export default Scanner;
+}
